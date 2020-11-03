6e287f55fa40bb097dbbec72f0e9d1fc7564957d
Diego Dagum <diego@aimi.fm>
2020-11-02 16:27:11 -0800

    Initial commit.
    So far, we got a native library with a native method getPlatformVersion() both in Kotlin and Swift.
    We also got, in the same library, a native method native_add_two_ints in C/C++.

---
+++ ios/Classes/SwiftNativeAddPlugin.swift
@@ -0,0 +1,14 @@
+import Flutter
+import UIKit
+
+public class SwiftNativeAddPlugin: NSObject, FlutterPlugin {
+  public static func register(with registrar: FlutterPluginRegistrar) {
+    let channel = FlutterMethodChannel(name: "platform_info", binaryMessenger: registrar.messenger())
+    let instance = SwiftNativeAddPlugin()
+    registrar.addMethodCallDelegate(instance, channel: channel)
+  }
+
+  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+    result("iOS " + UIDevice.current.systemVersion)
+  }
+}
